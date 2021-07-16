31e919f0e415f9cda8524fac6c4ab809aabddf18
Alexandre <alepateis9090@gmail.com>
2021-07-15 21:19:53 -0300

    retificado

--- activity_06.py
+++ activity_06.py
@@ -1,8 +1,8 @@
-list_01=[4,5,6,3,6,9,8,5]
+list_01=[4,5,6,3,6,9,8,5,5,8,74,1,3]
 list_02=[2,5,6,9,3,2,2]
 new_list = []
 
-def add_list(x, l):
+"""def add_list(x, l):
     if len(x)>len(l):
         for c in range(0, len(l)):
             new= x[c]+l[c]
@@ -15,4 +15,7 @@ def add_list(x, l):
         new = 0
     return new_list 
 
-print(add_list(list_01, list_02))
+print(add_list(list_01, list_02))"""
+
+new_list=[x + y for x, y in zip(list_01, list_02)]
+print(new_list)
