c045a8702137ff7cb780c98328dbabebf095d811
Alexandre <alepateis9090@gmail.com>
2021-07-19 22:05:17 -0300

    maps

--- activity_06.py
+++ activity_06.py
@@ -1,21 +1,17 @@
-list_01=[4,5,6,3,6,9,8,5,5,8,74,1,3]
+list_01=[10,5,6,3,6,9,8,5,5,8,74,1,3]
 list_02=[2,5,6,9,3,2,2]
 new_list = []
 
-"""def add_list(x, l):
+def add_list(x, l):
     if len(x)>len(l):
         for c in range(0, len(l)):
-            new= x[c]+l[c]
-            new_list.append(new)
-            new = 0
+            new_list.append(x[c]+l[c])
         return new_list        
     for c in range(0, len(x)):
-        new= x[c]+l[c]
-        new_list.append(new)
-        new = 0
+        new_list.append(x[c]+l[c])
     return new_list 
-
-print(add_list(list_01, list_02))"""
-
-new_list=[x + y for x, y in zip(list_01, list_02)]
+new_list = add_list(list_01, list_02)
 print(new_list)
+
+#new_list=[x + y for x, y in zip(list_01, list_02)]    #Forma mais simples
+#print(new_list)
---
+++ dictcomprehesion.py
@@ -0,0 +1,30 @@
+product = [
+    {'name':'p1', 'price': 50},
+    {'name':'p2', 'price': 22},
+    {'name':'p3', 'price': 33},
+    {'name':'p4', 'price': 47},
+    {'name':'p5', 'price': 2},
+    {'name':'p6', 'price': 18},
+    {'name':'p7', 'price': 36},
+    {'name':'p8', 'price': 28},
+    {'name':'p9', 'price': 19},
+    {'name':'p10', 'price': 37}
+]
+for p in product:
+    print(p)
+    new = [p['price']*2 for p in product]
+print()
+#print(new)
+i=0
+for p in product:
+    p['price'] = new[i]
+    i+=1
+for p in product:
+    print(p)            
+print()
+def more_20(p):
+    p['price'] = p['price'] * 4
+    return p
+new_price = list(map(more_20, product))
+for r in new_price:
+    print(r)
