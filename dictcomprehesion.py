b6d03a4517fb9dc01381627427c2ed1ed9c09068
Alexandre <alepateis9090@gmail.com>
2021-07-21 22:18:52 -0300

    aula erro

--- dictcomprehesion.py
+++ dictcomprehesion.py
@@ -1,4 +1,4 @@
-product = [
+'''product = [
     {'name':'p1', 'price': 50},
     {'name':'p2', 'price': 22},
     {'name':'p3', 'price': 33},
@@ -28,3 +28,29 @@ def more_20(p):
 new_price = list(map(more_20, product))
 for r in new_price:
     print(r)
+
+'''
+#Nova a partir daqui
+
+
+def check_if_number(num):
+    try:
+        num = int(num)
+        return num
+    except ValueError:
+        try:
+            num = float(num)
+            return num
+        except ValueError:
+            pass        
+
+
+
+
+while True:
+    number = check_if_number(input('Enter a number: '))
+    if number is None:
+        print('Erro: isso não é um número.')
+    else:
+        print(number)    
+
